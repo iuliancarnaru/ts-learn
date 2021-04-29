3c5f2c5bbd2f747db02ffb19b2520d860c87f7fe
iuliancarnaru <iulian.carnaru@gmail.com>
2021-04-29 13:22:39 +0100

    more interfaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,7 +1,10 @@
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name ${this.name}`;
+  },
 };
 
 // const printVehicle = (vehicle: {
@@ -16,16 +19,25 @@ const oldCivic = {
 
 // printVehicle(oldCivic);
 
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  // name: string;
+  // year: Date;
+  // broken: boolean;
+  summary(): string;
 }
 
-const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`Name: ${vehicle.name}`);
-  console.log(`Year: ${vehicle.year}`);
-  console.log(`Broken? ${vehicle.broken}`);
+const drink2 = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
 };
 
-printVehicle(oldCivic);
+const printReport = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printReport(oldCivic);
+printReport(drink2);
