b6d6e726ec8a78d6a523e6a81b6337249c69e672
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-06 12:49:41 +0100

    using enums

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,28 @@
-console.log('is working');
+// every time we use a build in node module
+// we have to install @types/node
+import fs from 'fs';
+
+// when adding encoding a string is returned instead of a buffer
+const matches = fs
+  .readFileSync('football.csv', { encoding: 'utf-8' })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+// enum - enumeration
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D',
+}
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+    manUnitedWins++;
+  } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
