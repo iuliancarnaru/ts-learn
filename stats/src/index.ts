5cdf5980bbc9cb961abb63f3ba0ba81a17f4963a
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-07 15:28:11 +0100

    refactoring using interfaces

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,12 +1,18 @@
 import { MatchResult } from './MatchResult';
 import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './CsvFileReader';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// create an object that satisfies 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// create an instance of 'MatchReader' and pass in something
+// that satisfies 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
--- stats/src/inheritance/CsvFileReader.ts
+++ stats/src/inheritance/CsvFileReader.ts
@@ -2,16 +2,20 @@
 // we have to install @types/node
 import fs from 'fs';
 
-export class CsvFileReader {
-  data: string[][] = [];
+// <T> is a generic type
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
+
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     // when adding encoding a string is returned instead of a buffer
     this.data = fs
       .readFileSync(this.filename, { encoding: 'utf-8' })
       .split('\n')
-      .map((row: string): string[] => row.split(','));
+      .map((row: string): string[] => row.split(','))
+      .map(this.mapRow);
   }
 }
