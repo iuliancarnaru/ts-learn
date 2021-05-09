80051aa5a8bb3e55fe15d9841b9b8a59b3c63eb7
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-09 22:12:55 +0100

    adding user class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,1 +1,8 @@
-console.log('Working');
+import { User } from './models/User';
+
+const user = new User({});
+
+user.set({ name: 'Iulian', age: 36 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
---
+++ web/src/models/User.ts
@@ -0,0 +1,17 @@
+// adding '?' we make it optional
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+export class User {
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+}
