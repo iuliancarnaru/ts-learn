222af59dee74c83f3537018707675f56af42459b
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-10 21:58:45 +0100

    mega user class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,16 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({});
+const user = new User({ id: 1 });
 
-user.on('change', () => {
-  console.log('Change #1');
-});
-user.on('change', () => {
-  console.log('Change #2');
-});
-user.on('click', () => {
-  console.log('Click #1');
-});
+user.set({ name: 'Carnaru', age: 37 });
+user.save();
 
-user.trigger('change');
-user.trigger('click');
+const user2 = new User({ name: 'Aneta', age: 28 });
+user2.save();
