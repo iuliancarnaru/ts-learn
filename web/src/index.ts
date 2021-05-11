2a255044a0200df5a98e077df3c2decc99179b4f
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-11 08:49:04 +0100

    creating eventing class

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,9 +1,13 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'Carnaru', age: 37 });
 
 user.set({ name: 'Carnaru', age: 37 });
 user.save();
 
-const user2 = new User({ name: 'Aneta', age: 28 });
-user2.save();
+user.events.on('change', () => {
+  console.log('change');
+});
+
+// const user2 = new User({ name: 'Aneta', age: 28 });
+// user2.save();
--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from 'axios';
+import { Eventing } from './Eventing';
 
 // adding '?' we make it optional
 interface UserProps {
@@ -7,10 +8,8 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public events: Eventing = new Eventing();
 
   constructor(private data: UserProps) {}
 
@@ -20,23 +19,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 
   fetch(): void {
