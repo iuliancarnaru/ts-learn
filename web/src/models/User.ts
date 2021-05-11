8b82f8fd7dc464a21eb2a6d2e2d3b0311fb51ebd
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-11 22:19:04 +0100

    converting to generic class

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,8 +1,7 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
 // adding '?' we make it optional
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
@@ -19,25 +18,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      // put
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      // post
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
