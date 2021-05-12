038e2a395e19c6059b659210bd4a407b09af67c6
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-12 17:13:41 +0100

    composition style approach

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,9 @@
 import { User } from './models/User';
 
-const user = new User({ name: 'Carnaru', age: 37 });
+const user = new User({ id: 1, name: 'Iulian', age: 36 });
 
-console.log(user.get('name'));
-
-user.on('change', () => {
-  console.log('user was changed');
+user.on('save', () => {
+  console.log(user);
 });
 
-user.set({ name: 'Iulian' });
+user.save();
