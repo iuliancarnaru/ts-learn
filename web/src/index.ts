1a7aa8bf51c5c7a5a57d6fd752af9209ab78c757
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-14 22:52:04 +0100

    rendering

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,4 +1,7 @@
 import { UserForm } from './views/UserForm';
+import { User } from './models/User';
 
-const userForm = new UserForm(document.getElementById('root'));
+const user = User.buildUser({ name: 'Aneta', age: 28 });
+
+const userForm = new UserForm(document.getElementById('root'), user);
 userForm.render();
