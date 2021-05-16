2015570719b3422c9ab2f47027f28c68c6730abe
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-16 13:39:50 +0100

    added tsconfig file

--- web/src/index.ts
+++ web/src/index.ts
@@ -3,5 +3,11 @@ import { User } from './models/User';
 
 const user = User.buildUser({ name: 'Aneta', age: 28 });
 
-const userForm = new UserForm(document.getElementById('root'), user);
-userForm.render();
+const root = document.getElementById('root');
+
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else {
+  throw new Error('Root element not found');
+}
