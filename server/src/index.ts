50fff4198850afe6d83485b1138f1f4257412761
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-18 09:54:00 +0100

    adding routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,15 @@
-console.log('hi there');
+import express from 'express';
+import { router } from './routes/loginRoutes';
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(router);
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
---
+++ server/src/routes/loginRoutes.ts
@@ -0,0 +1,23 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get('/login', (req: Request, res: Response) => {
+  res.send(`
+    <form method="POST">
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  `);
+});
+
+router.post('/login', (req: Request, res: Response) => {});
+
+export { router };
