2fb3e6fc44333822d01ab4aec3fc58e41660196c
iuliancarnaru <iulian.carnaru@gmail.com>
2021-05-20 23:54:33 +0100

    render todos

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -9,12 +9,25 @@ interface AppProps {
 }
 
 class _App extends Component<AppProps> {
-  componentDidMount() {
+  onButtonClick = (): void => {
     this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
   }
 
   render() {
-    return <div>Hi there</div>;
+    return (
+      <div>
+        <button type="button" onClick={this.onButtonClick}>
+          Fetch data
+        </button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
